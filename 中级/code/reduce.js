85281b222ca2f6bca351c49c14ba82525c9e8b90
xywang71 <xywang71@iflytek.com>
2022-11-15 18:31:17 +0800

    feat: 手写reduce

---
+++ 中级/code/reduce.js
@@ -0,0 +1,19 @@
+function reduce(list, fn, ...init) {
+    let next = init.length ? init[0] : list[0]
+    let i = init.length ? 0 : 1;
+    for (; i < list.length; i++) {
+        next = fn(next, list[i], i, list)
+    }
+    return next;
+}
+
+// => 55
+const a = reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (x, y) => x + y)
+
+// => 155
+const b = reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (x, y) => x + y, 100)
+
+// => NaN
+const c = reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (x, y) => x + y, undefined)
+
+console.log(a, b, c)
